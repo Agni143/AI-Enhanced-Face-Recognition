595454a4d926599891f900925b6097a6a38f81de
Tribhuvan <113534464+Tribhuvan2025@users.noreply.github.com>
2025-01-19 16:20:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from home_page import home_page
 init_db()
 
 # Streamlit Page Config
-st.set_page_config(page_title = 'Student Verification', layout='wide')
+st.set_page_config(page_title = 'Student Verification')
 
 # Add background image to the main page
 
